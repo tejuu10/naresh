76eaca06815cbf234e15bfe4dd0bb4955984afdc
Tejuu <tejuvislavath10@gmail.com>
2025-04-09 14:02:54 +0000

    Update index.java

--- index.java
+++ index.java
@@ -1,17 +1,7 @@
 import java.util.*;
 class index{
   public static void main(String a[]){
-   int n,i,count=0;
-   System.out.println("Enter number");
-   Scanner sc=new Scanner(System.in);
-   n=sc.nextInt();
-   for(i=1;i<=n;i++){
-      if(n%i==0)
-      count++;
-  }
-  if(count==2)
-  System.out.println(n+" is prime");
-  else
+  
    System.out.println(n+" is not  prime");
   }
 }
